9efc2a1f91c38eb8f43a374c9603f546608beaee
mintiaaa <nsugi1010@gmail.com>
2023-09-28 03:26:55 +0900

    remove unused lines

--- interface/include/customer_rule.h
+++ interface/include/customer_rule.h
@@ -5,40 +5,10 @@
 #include <iostream>
 #include <memory>
 
-class Date
-{
-protected:
-    int month_;
-    int day_;
-    int hour_;
-    int minute_;
-    int second_;
-public:
-    Date(const int month, const int day, const int hour, const int minute, const int second)
-    {
-        if(month_ < 1 || month_ > 12){
-            throw std::invalid_argument("[invalid argument error] month must be between 1 and 12");
-        }
-        if(day_ < 1 || day_ > 31){
-            throw std::invalid_argument("[invalid argument error] day must be between 1 and 31");
-        }
-        if(hour_ < 0 || hour_ > 24){
-            throw std::invalid_argument("[invalid argument error] hour must be between 0 and 24");
-        }
-        if(minute_ < 0 || minute_ > 60){
-            throw std::invalid_argument("[invalid argument error] minute must be between 0 and 60");
-        }
-        if(second_ < 0 || second_ > 60){
-            throw std::invalid_argument("[invalid argument error] second must be between 0 and 60");
-        }
-        month_ = month;
-        day_ = day;
-        hour_ = hour;
-        minute_ = minute;
-        second_ = second;
-    }
-};
-
+/**
+ * @brief 顧客情報
+ *
+ */
 class PurchaseHistory
 {
 public:
